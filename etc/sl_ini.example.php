9725a33ab1029a5c233e90cc9098a3b29746e3e3
yU8azC3bq <112607301+yU8azC3bq@users.noreply.github.com>
2025-02-08 17:20:58 +0100

    重构了 main.py 代码

---
+++ etc/sl_ini.example.php
@@ -0,0 +1,36 @@
+<?php
+
+// The web root. The path that can be used when linking (to js, css, ...)
+// This should probably be something like http://localhost/stacklife
+$www_root = "http://example.com/stacklife";
+
+// Build version
+$version = "alpha 2.3";
+
+// DB connection details
+$hostName = "localhost";
+$userName = "youdbusername";
+$pw = "yourdbpassword";
+
+// Availability
+$AVAILABILITY_URL = "youravailurl";
+
+// LibraryCloud
+// Probably something like http://example.com/platform/v0.03/api/item/
+$LIBRARYCLOUD_URL = 'http://example.com/platform/v0.03/api/item/';
+
+// Probably someting like http://example.com/platform/v0.03/api/item/
+$LIBRARYCLOUD_KEY = 'yourlckey';
+
+$TYPEKIT_CODE = 'yourtypekitcode';
+
+// Amazon
+$AMAZON_KEY = 'youramazonkey';
+$AMAZON_SECRET_KEY = 'youramazonsecretkey';
+$AMAZON_ASSOC_TAG = 'youramazonassoctag';
+
+// One or more Google Analytics accounts
+$GOOGLE_ANALYTICS = array("UA-XXXXXX-X");
+$GOOGLE_ANALYTICS_DOMAIN = "example.com";
+
+?>
